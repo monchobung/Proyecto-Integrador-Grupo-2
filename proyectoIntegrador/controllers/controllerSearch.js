3cb674cb3da2500f7580586c552516e7709de1a7
ramiporrotoyos <rporrotoyos@udesa.edu.ar>
2025-04-11 09:50:35 -0300

    termine le search resutls

--- proyectoIntegrador/controllers/controllerSearch.js
+++ proyectoIntegrador/controllers/controllerSearch.js
@@ -1,6 +1,12 @@
+const datos = require("../db/index");
+
 const controllerSearch = {
     index: function (req, res) {
-        res.render('searchResults')
+        res.render('searchresults',{
+            title: 'Producto',
+            usuario: datos.usuario,
+            productos: datos.productos,
+        })
     },
 }
 
