4df142fab19b6df4387ecd4c6797c99f2c9da20a
ccamdessus <ccamdessus@udesa.edu.ar>
2025-04-12 12:23:15 -0300

    que el form de headerlogueado te lleve a searchresults y titulo de la pagina

--- proyectoIntegrador/controllers/controllerSearch.js
+++ proyectoIntegrador/controllers/controllerSearch.js
@@ -3,9 +3,10 @@ const datos = require("../db/index");
 const controllerSearch = {
     index: function (req, res) {
         res.render('searchresults',{
-            title: 'Producto',
+
             usuario: datos.usuario,
             productos: datos.productos,
+            
         })
     },
 }
